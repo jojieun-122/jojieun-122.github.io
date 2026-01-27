8ad79c5cbd2946e5faaf5b114fc1a7d629f222da
jojieun122 <roseolivia435531@gmail.com>
2026-01-27 22:48:54 +0900

    Project

---
+++ Javascript/datatype.js
@@ -0,0 +1,50 @@
+//String Data Type
+
+let name = 'Phyu Sin Phoo Pwint';
+console.log(name);
+console.log(typeof(name));
+
+// numeric data type
+let age = 16;
+console.log(age);
+console.log(typeof(age));
+
+// bollen data type 0,1
+let single = true;
+console.log(single);
+console.log(typeof(single));
+
+
+// undifined
+let myGimseokjin;
+console.log(myGimseokjin);
+
+
+// null
+let son = null;
+console.log(son);
+console.log(typeof(son));
+
+//array
+let arr1 = new Array('a','b','c');
+console.log(arr1)
+console.log(typeof(arr1));
+
+let arr2 = [1,2,3,4,5]
+console.log(arr2)
+console.log(typeof(arr2));
+
+//multiarry
+let multi = ['Mg Mg', 'Aung Aung', 'Hla Hla'['Tun Tun', 'Mya Mya'],'Aye Aye'];
+console.log(multi)
+console.log(typeof(multi));
+
+//Object
+let student ={
+    //key : value ,
+    name : "Phoo Phoo",
+    age : 16,
+    address : "Yangon",
+    eduaction : "High School"
+}
+console.log(student);
